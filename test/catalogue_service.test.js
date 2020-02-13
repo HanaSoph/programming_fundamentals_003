b87d534c45c5b1c29e084b5838db4a6fe2218723
Hannah <hannahbeardmore1@hotmail.co.uk>
2020-02-13 18:46:26 +0000

    gone through and completed programming fundamentals 3

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -8,4 +8,69 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true if the book exists", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "The Assassination of Margaret Thatcher"
+        )
+      ).toBe(true);
+    });
+    test("returns false if the book does not exist", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "The Chronicles of Narnia"
+        )
+      ).toBe(false);
+    });
+  });
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns books which start with the given letter", () => {
+      expect(
+        catalogueService.countBooksByFirstLetter(
+          "W"
+        )
+      ).toBe(2);
+    });
+  });
+  describe("catalogueService.getQuantity", () => {
+    test("returns the stock number", () => {
+      expect(
+        catalogueService.getQuantity(
+          "The Catcher in the Rye"
+        )
+      ).toBe(10);
+    });
+  });
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns the books by a specific author", () => {
+      expect(
+        catalogueService.getBooksByAuthor(
+          "Robert Bolaño"
+        )
+      ).toEqual(
+        [
+          { title: "2666", author: "Robert Bolaño", quantity: 17 },
+          { title: "By Night In Chile", author: "Robert Bolaño", quantity: 8 }
+        ]
+      );
+    });
+  });
+  describe("catalogueService.checkQuantity", () => {
+    test("returns true if in stock or false if not", () => {
+      expect(
+        catalogueService.checkQuantity(
+          "By Night In Chile", 4
+        )
+      ).toBe(true);
+    });
+    test("returns true if in stock or false if not", () => {
+      expect(
+        catalogueService.checkQuantity(
+          "By Night In Chile", 100
+        )
+      ).toBe(false);
+  });
+});
 });
